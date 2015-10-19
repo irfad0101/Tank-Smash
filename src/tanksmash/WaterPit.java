9b31fb1d1bb9ee28587afd50eb6fe79ad180cbd0
Irfad Hussain <irfad0101@gmail.com>
2015-10-19 22:14:40 +0530

    Add entity classes

---
+++ src/tanksmash/WaterPit.java
@@ -0,0 +1,17 @@
+
+package tanksmash;
+
+import java.awt.image.BufferedImage;
+
+/*
+ * @author Irfad Hussain
+ */
+public class WaterPit extends GameObject{
+    
+    private static BufferedImage waterPit;
+
+    public WaterPit(int x, int y) {
+        super(x, y);
+    }
+    
+}
