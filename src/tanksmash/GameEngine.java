ff561d5c0257988b36cd46491ce08bf64a1c136f
Irfad Hussain <irfad0101@gmail.com>
2015-10-25 15:02:49 +0530

    add fields for GameEngine.java

--- src/tanksmash/GameEngine.java
+++ src/tanksmash/GameEngine.java
@@ -3,8 +3,42 @@ package tanksmash;
 
 public class GameEngine {
     
+    private boolean gameStarted;
+    private boolean gameFinished;
+    private boolean alive;
+    
+    public GameEngine(){
+        this.gameStarted = false;
+        this.gameFinished = false;
+        this.alive = false;
+    }
+    
     public void decode(String message){
         
+    }
+    
+    public boolean isGameStarted(){
+        return gameStarted;
+    }
+    
+    public void setGameStarted(boolean gameStarted){
+        this.gameStarted = gameStarted;
+    }
+    
+    public boolean isGameFinished(){
+        return gameFinished;
+    }
+    
+    public void setGameFinsished(boolean gameFinished){
+        this.gameFinished = gameFinished;
+    }
+    
+    public boolean isAlive(){
+        return alive;
+    }
+    
+    public void setAlive(boolean alive){
+        this.alive = alive;
     }
     
 }
