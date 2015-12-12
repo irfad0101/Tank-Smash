e95aed0de334ceb1b3537f1e449ce9b304105d9a
sachithra sahan <sachithrasahansamarasinghe@gmail.com>
2015-12-12 13:10:07 +0530

    edit the basics of game window

--- TankSmashXNA/TankSmashXNA/TankSmashXNA/Game1.cs
+++ TankSmashXNA/TankSmashXNA/TankSmashXNA/Game1.cs
@@ -18,6 +18,13 @@ namespace TankSmashXNA
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Texture2D gridTexture;
+        Texture2D brickTexture;
+        Texture2D waterTexture;
+        Texture2D tankTexture;
+        Texture2D lifePackTexture;
+        Texture2D stoneTexture;
+
 
         public Game1()
         {
@@ -34,7 +41,11 @@ namespace TankSmashXNA
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            graphics.PreferredBackBufferWidth = 500;
+            graphics.PreferredBackBufferHeight = 500;
+            graphics.IsFullScreen = false;
+            graphics.ApplyChanges();
+            Window.Title = "Tank Game - Client";
             base.Initialize();
         }
 
@@ -46,6 +57,12 @@ namespace TankSmashXNA
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            gridTexture = Content.Load<Texture2D>("grid");
+            brickTexture = Content.Load<Texture2D>("brick");
+            waterTexture = Content.Load<Texture2D>("water");
+            tankTexture = Content.Load<Texture2D>("Tank");
+            lifePackTexture = Content.Load<Texture2D>("LifePack");
+            stoneTexture = Content.Load<Texture2D>("stone");
 
             // TODO: use this.Content to load your game content here
         }
@@ -81,11 +98,62 @@ namespace TankSmashXNA
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.White);
 
             // TODO: Add your drawing code here
-
+            spriteBatch.Begin();
+            DrawBackground();
+            DrawBrick();
+            DrawWater();
+            DrawTank();
+            DrawLifePack();
+            DrawStone();
+            spriteBatch.End();
             base.Draw(gameTime);
+        }
+
+        private void DrawBackground()
+        {
+            Rectangle screenRectangle = new Rectangle(0,0,500,500);
+            spriteBatch.Draw(gridTexture, screenRectangle, Color.White);
+            /*for (int i = 0; i < 10; i++)
+			{
+                for (int j = 0; j < 10; j++)
+                {
+                    Rectangle screenRectangle = new Rectangle(50*i,50*j, 55,55);
+                    spriteBatch.Draw(gridTexture, screenRectangle, Color.White);
+                }
+             }*/
+            
+        }
+
+        private void DrawBrick()
+        {
+            Rectangle brickRectangle = new Rectangle(51, 1, 50, 50);
+            spriteBatch.Draw(brickTexture, brickRectangle, Color.White);
+        }
+
+        private void DrawWater()
+        {
+            Rectangle waterRectangle = new Rectangle(51, 101, 50, 50);
+            spriteBatch.Draw(waterTexture, waterRectangle, Color.White);
+        }
+
+        private void DrawTank()
+        {
+            Rectangle tankRectangle = new Rectangle(101, 101, 50, 50);
+            spriteBatch.Draw(tankTexture, tankRectangle, Color.White);
+        }
+        private void DrawLifePack()
+        {
+            Rectangle lifePackRectangle = new Rectangle(201, 201, 50, 50);
+            spriteBatch.Draw(lifePackTexture, lifePackRectangle, Color.White);
+        }
+
+        private void DrawStone()
+        {
+            Rectangle stoneRectangle = new Rectangle(250, 250, 50, 50);
+            spriteBatch.Draw(stoneTexture, stoneRectangle, Color.White);
         }
     }
 }
