6e0312470f2dd7feb6e0379a3412685bef3173e7
Irfad Hussain <irfad0101@gamil.com>
2015-12-12 16:25:45 +0530

    Add NetworkHandler.cs

---
+++ TankSmashXNA/TankSmashXNA/TankSmashXNA/NetworkHandler.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TankSmashXNA
+{
+    class NetworkHandler
+    {
+
+        private static NetworkHandler netHandler = new NetworkHandler();
+        private TcpClient sender;
+        private NetworkStream sendStream;
+        private BinaryWriter writer;
+        private TcpListener reciever;
+        private NetworkStream recieveStream;
+        private BinaryReader reader;
+
+        private NetworkHandler(){}
+
+        public static NetworkHandler getInstance(){
+            return netHandler;
+        }
+
+        public void Send(String message)
+        {
+            try
+            {
+                sender = new TcpClient();
+                sender.Connect("127.0.0.1", 6000);
+                if (sender.Connected)
+                {
+                    sendStream = sender.GetStream();
+                    writer = new BinaryWriter(sendStream);
+                    Byte[] msg = Encoding.ASCII.GetBytes(message);
+                    writer.Write(msg);
+                    Console.WriteLine("Request Sent:"+message);
+                    writer.Close();
+                    sendStream.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                sender.Close();
+            }
+        }
+
+        public void Recieve()
+        {
+            Socket socket = null;
+            bool error = false;
+            try
+            {
+                reciever = new TcpListener(IPAddress.Parse("127.0.0.1"), 7000);
+                reciever.Start();
+                while (true)
+                {
+                    socket = reciever.AcceptSocket();
+                    if (socket.Connected)
+                    {
+                        recieveStream = new NetworkStream(socket);
+                        List<Byte> serverMsg = new List<byte>();
+                        int asw = 0;
+                        while (asw != -1)
+                        {
+                            asw = recieveStream.ReadByte();
+                            serverMsg.Add((Byte)asw);
+                        }
+                        String message = Encoding.UTF8.GetString(serverMsg.ToArray());
+                        Console.WriteLine(message);
+                        recieveStream.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                error = true;
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (socket != null)
+                {
+                    if (socket.Connected)
+                        socket.Close();
+                }
+                if (error)
+                    Recieve();
+            }
+        }
+
+    }
+}
